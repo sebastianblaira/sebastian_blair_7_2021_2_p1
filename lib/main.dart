dd5a39e2ca8ed4ab0e046e264aef0129624b3112
sebastianblaira <sebastianblaira1996@gmail.com>
2021-10-23 12:40:34 -0500

    add meme detail

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:memes_app/pages/home_page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
--- lib/pages/detail_meme_page.dart
+++ lib/pages/detail_meme_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:memes_app/models/meme_model.dart';
 
@@ -16,13 +17,87 @@ class DetailsMemePage extends StatelessWidget {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image(
-            image: NetworkImage(meme.submissionUrl),
-            height: 200,
-            fit: BoxFit.cover,
+          Container(
+            height: 250,
+            width: 250,
+            margin: const EdgeInsets.only(top: 40),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(80),
+              image: DecorationImage(
+                  image: NetworkImage(meme.submissionUrl), fit: BoxFit.cover),
+            ),
           ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: Column(
+              children: [
+                Text(
+                  meme.submissionTitle,
+                  style: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      'Autor',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(meme.author)
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      'Fecha de creación',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(meme.created.toString())
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      'Link',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      child: Text(
+                        meme.permalink.substring(0, 50),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
